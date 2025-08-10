8484aeebce8abd4beb70e9edd96323ea9ce0300c
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-08-10 10:46:17 +0900

    style: mapPinの共通化

--- frontend/src/components/MapPin.tsx
+++ frontend/src/components/MapPin.tsx
@@ -12,16 +12,20 @@ type MapPinProps = {
 const MapPin = ({ pins, setSelectedPin }: MapPinProps) => {
   return (
     <>
-      {pins.map((pin) => (
-        <Marker
-          key={pin.id}
-          onPress={() => setSelectedPin(pin)}
-          coordinate={{
-            latitude: pin.lat,
-            longitude: pin.lon,
-          }}
-        >
-          {pin.iconUrl ? (
+      {pins.map((pin) => {
+        const iconSource = pin.iconUrl
+          ? { uri: pin.iconUrl }
+          : require("../assets/images/no-profile.png");
+
+        return (
+          <Marker
+            key={pin.id}
+            onPress={() => setSelectedPin(pin)}
+            coordinate={{
+              latitude: pin.lat,
+              longitude: pin.lon,
+            }}
+          >
             <View>
               <View style={pinStyle.bubbleContainer}>
                 <View style={pinStyle.bubble}>
@@ -33,35 +37,13 @@ const MapPin = ({ pins, setSelectedPin }: MapPinProps) => {
               </View>
               <View style={pinStyle.markerContainer}>
                 <View style={pinStyle.iconContainer}>
-                  <Image
-                    source={{ uri: pin.iconUrl }}
-                    style={pinStyle.markerImage}
-                  />
+                  <Image source={iconSource} style={pinStyle.markerImage} />
                 </View>
               </View>
             </View>
-          ) : (
-            <View>
-              <View style={pinStyle.bubbleContainer}>
-                <View style={pinStyle.bubble}>
-                  <Text style={pinStyle.text}>
-                    {TruncateMessage(pin.message)}
-                  </Text>
-                </View>
-                <View style={pinStyle.arrow} />
-              </View>
-              <View style={pinStyle.markerContainer}>
-                <View style={pinStyle.iconContainer}>
-                  <Image
-                    source={require("../assets/images/no-profile.png")}
-                    style={pinStyle.markerImage}
-                  />
-                </View>
-              </View>
-            </View>
-          )}
-        </Marker>
-      ))}
+          </Marker>
+        );
+      })}
     </>
   );
 };
