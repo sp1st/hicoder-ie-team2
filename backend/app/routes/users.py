bcd1fd26102d7ec530e181fb881661457f593060
LAPTOP-DISIE3P5\shuny <i20004@e.hiroshima-cu.ac.jp>
2025-08-10 10:53:44 +0900

    add feature/backend-userNearbyGET

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.models import User
+from app.models import User,WaterRecord
 from app import db
 
 users_bp = Blueprint('users', __name__)
@@ -52,3 +52,40 @@ def update_user(user_id):
     'X' : user.X,
     'photo_url' : user.photo_url
   })
+
+# GET /api/v1/users/nearby/<user_id>
+#　近くのユーザーのIDと位置情報を取得
+@users_bp.route('/nearby/<int:user_id>', methods=['GET'])
+def get_nearby_users(user_id):
+
+  #IDからWaterRecordを取得
+  record = WaterRecord.query.filter_by(user_id=user_id).order_by(WaterRecord.water_date.desc()).first()
+
+  if not record:
+    return jsonify({'message': 'No water record found for this user', 'user_id': user_id}), 404
+  
+  # WaterRecordから緯度経度を取得
+  lat = record.lat
+  lon = record.lon
+
+  # 近くの記録を検索
+  nearby_records = WaterRecord.query.filter(
+      WaterRecord.lat.between(lat - 0.01, lat + 0.01),
+      WaterRecord.lon.between(lon - 0.01, lon + 0.01),
+      WaterRecord.user_id != user_id
+  ).distinct(WaterRecord.user_id, WaterRecord.water_id).all()
+
+  if not nearby_records:
+    return jsonify({'message': 'No nearby water records found', 'user_id': user_id}), 404
+  
+  # ユーザー情報をJSON形式で返す
+  records = []
+  for info in nearby_records:
+    info_data = {
+      'user_id': info.user_id,
+      'lat': info.lat,
+      'lon': info.lon
+    }
+    records.append(info_data)
+
+  return jsonify(records)
